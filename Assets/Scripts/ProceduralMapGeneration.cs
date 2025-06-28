c642b70754e5c487c47620ce5253dbc40a5a4597
DoTaeIn <hirobin0412@gmail.com>
2025-06-28 19:40:29 +0900

    Fix merge Error

--- Assets/Scripts/ProceduralMapGeneration.cs
+++ Assets/Scripts/ProceduralMapGeneration.cs
@@ -32,46 +32,36 @@ public class ProceduralMapGeneration : MonoBehaviour
 
         for (int i = 0; i < numberOfRooms; i++)
         {
-            if (i != toiletSeed)
-            {
-                GameObject roomGO = Instantiate(roomPrefabs[Random.Range(0, roomPrefabs.Length)], new Vector3(0, 0, 0), Quaternion.identity);
-
-                Room room = roomGO.GetComponent<Room>();
-                int x = (i % 5) * 20 + 5;
-                int y = Mathf.FloorToInt(i / 5) * 20 + 5;
-
-                room.InitRoom(i, x, y);
-                roomManager.RegisterRoom(i, room);
-                //Debug.Log($"Room {i} position: {x}x{y}");
-            }
-            else
-            {
-                GameObject Toilet = Instantiate(roomPrefabs[Random.Range(0, roomPrefabs.Length)], new Vector3(0, 0, 0), Quaternion.identity);
-                Toilet.name = "Toilet";
-                Room toilet = Toilet.GetComponent<Room>();
-                int toileti = i;
-                roomManager.RegisterRoom(i, toilet);
-                toilet.CreateToilet(toileti, (toileti % 5) * 20 + 5, Mathf.FloorToInt(toileti / 5) * 20 + 5);
-            }
-
-        }
-
             int x = (i % 5) * 20 + 5;
             int y = Mathf.FloorToInt(i / 5) * 20 + 5;
 
-        GameObject LivingRoom = Instantiate(roomPrefabs[Random.Range(0, roomPrefabs.Length)], new Vector3(0, 0, 0), Quaternion.identity);
+            if (i != toiletSeed)
+            {
+                GameObject roomGO = Instantiate(roomPrefabs[Random.Range(0, roomPrefabs.Length)], Vector3.zero, Quaternion.identity);
+                Room room = roomGO.GetComponent<Room>();
+                room.InitRoom(i, x, y);
+                roomManager.RegisterRoom(i, room);
+                room.SetupDoors();
+            }
+            else
+            {
+                GameObject Toilet = Instantiate(roomPrefabs[Random.Range(0, roomPrefabs.Length)], Vector3.zero, Quaternion.identity);
+                Toilet.name = "Toilet";
+                Room toilet = Toilet.GetComponent<Room>();
+                toilet.CreateToilet(i, x, y);
+                roomManager.RegisterRoom(i, toilet);
+                toilet.SetupDoors();
+            }
+        }
+
+        GameObject LivingRoom = Instantiate(roomPrefabs[Random.Range(0, roomPrefabs.Length)], Vector3.zero, Quaternion.identity);
         LivingRoom.name = "LivingRoom";
         Room livingRoom = LivingRoom.GetComponent<Room>();
         livingRoom.GenerateLivingRoom(35, -20, 30, 20);
         roomManager.RegisterRoom(-3, livingRoom);
         livingRoom.SetupDoors();
-
-
-        }
-
-        room.SetupDoors();
-
     }
+
     
     
 }
